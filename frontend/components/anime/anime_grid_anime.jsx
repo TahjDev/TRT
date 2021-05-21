b38671db98282a932e647d0f2d7584639ca7bdef
tahj <tahjharris00@gmail.com>
2021-05-21 09:39:53 -0500

    done

--- frontend/components/anime/anime_grid_anime.jsx
+++ frontend/components/anime/anime_grid_anime.jsx
@@ -5,15 +5,16 @@ import { Link } from "react-router-dom"
 const AnimeGridItem = (props) => {
     
     return (
-       
+    <Link to={`/animes/${props.anime.id}`}>
         <div className="anime-grid-bkg-img" style={{
             backgroundImage:
                 `url(${props.anime.backgroundPhoto})`
         }}>
-            <div className="anime-grid-img"><Link to={`/animes/${props.anime.id}`}><img src={props.anime.photoUrl} /></Link></div>
+            <div className="anime-grid-img"><img src={props.anime.photoUrl} /></div>
 
             <div className="lower-div"><h1>{props.anime.name}</h1></div>
         </div>
+    </Link>
     )
 }
 
