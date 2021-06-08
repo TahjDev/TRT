c00212151333e2f491c68187f12c6fd7b32d427b
tahj <tahjharris00@gmail.com>
2021-06-07 22:44:03 -0500

    work on linkedin posting

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,6 +5,35 @@ import { faGithub } from '@fortawesome/free-solid-svg-icons'
 const footer = () => {
     return (
         <div className="footer"> 
+            <div className="other-skills" >
+                <h1>Skills</h1>
+                React
+                <br/>
+                Ruby on Rails
+                <br />
+                Ruby
+                <br />
+                Javascript
+                <br />
+                WebSockets
+                <br />
+                AWS
+                <br />
+                CSS
+                <br />
+                SCSS
+                <br />
+                HTML
+                <br />
+                HTML-5
+            </div>
+            
+            <div className="other-projects" >
+               <h1>Other Projects</h1>
+               <a href="http://weeasel.herokuapp.com/">Weeasel</a>
+               <br/>
+               <a href="">Blessed Rain</a>
+            </div>
             <div className = "contact">
                 <h1>Contact Me</h1>
                 <div>
@@ -16,7 +45,7 @@ const footer = () => {
             <div className = "profiles">
                 <h1>Profiles</h1>
                 <div>
-                    <a href="">Github Account</a>
+                    <a href=""><i class="fab fa-github"></i> Github </a>
                     <br />
                     <a href="">LinkedIn Account</a>
                 </div>
--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faYoutubeSquare, faSearch, faSortDown} from '@fortawesome/free-solid-svg-icons'
+import {faSearch, faSortDown} from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom"
 
 class NavBar extends React.Component {
